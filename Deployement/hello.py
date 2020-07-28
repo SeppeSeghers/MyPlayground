cd9f128af4668782e67a2d006436512aafbdfc57
SeppeSeghers <seppeseghers1@gmail.com>
2020-07-28 15:31:01 +0200

    ??

--- Deployement/hello.py
+++ Deployement/hello.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'Hello World'
+    return 'Hello World'
 
 @app.route('/<name>')
 def user(name):
     return f"Hello {name}!"
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
